58cf24c221ac5d4b600bda1cd592d19082db755f
laurazp <laurikazp@gmail.com>
2022-12-11 10:41:51 +0000

    Lottie animation added and working

--- app/src/main/java/com/example/quake/SplashScreen.kt
+++ app/src/main/java/com/example/quake/SplashScreen.kt
@@ -5,46 +5,42 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val animationView: View = findViewById(R.id.animationView)
         val textView: TextView = findViewById(R.id.SplashScreenText)
         textView.visibility = View.GONE
-        //val view: View = findViewById(R.id.SplashScreenView)
-        //view.visibility = View.GONE
 
-        //Animation call
-        //val animation = AnimationUtils.loadAnimation(this, R.anim.quake_animation)
-
-        //Setting the animation
-        //view.setAnimation(animation)
-
-        // Hide status bar and make splash screen as a full screen activity
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-        /*Handler(Looper.getMainLooper()).postDelayed(
+        Handler(Looper.getMainLooper()).postDelayed(
             {
-            textView.visibility = View.VISIBLE
+                //Text fade in animation
+                textView.visibility = View.VISIBLE
+                val fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in_text_animation)
+                textView.setAnimation(fadeInAnimation)
             },
-            3000 // delayed time in milliseconds
-        )*/
+            2000)
 
-        /*Handler(Looper.getMainLooper()).postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            //Text fade out animation
+            textView.visibility = View.GONE
+            val fadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out_animation)
+            textView.setAnimation(fadeOutAnimation)
+            animationView.setAnimation(fadeOutAnimation)
+        }, 5000)
+
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            //finish()
-        }, 5500) // delayed time in milliseconds*/
+            finish()
+        }, 6500)
     }
 }
