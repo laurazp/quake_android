0a2692ff68c40ea5bbe8be6b348525350d4ea32c
laurazp <laurikazp@gmail.com>
2023-01-28 00:29:53 +0000

    Retrofit working and fetching earthquakes (changes to Dates needed)

--- app/src/main/java/com/example/quake/API/Models/Property.kt
+++ app/src/main/java/com/example/quake/API/Models/Property.kt
@@ -1,12 +1,13 @@
 package com.example.quake.API.Models
 
 import com.google.gson.annotations.SerializedName
+import java.sql.Time
 import java.util.Date
 
 data class Property (
     @SerializedName("mag") var mag: Double?,
     @SerializedName("place") var place: String?,
-    @SerializedName("time") var time: Date?, //TODO: Search for date/time in Android
+    //@SerializedName("time") var time: Date?, //TODO: Search for date/time in Android
     @SerializedName("tsunami") var tsunami: Int?,
     @SerializedName("title") var title: String?
     )
--- app/src/main/java/com/example/quake/Earthquakes.kt
+++ app/src/main/java/com/example/quake/Earthquakes.kt
@@ -1,7 +1,10 @@
 package com.example.quake
 
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +18,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class Earthquakes : AppCompatActivity(), OnQueryTextListener {
@@ -23,21 +29,17 @@ class Earthquakes : AppCompatActivity(), OnQueryTextListener {
     private lateinit var earthquakeAdapter: EarthquakeAdapter
     private val featureList = mutableListOf<Feature>()
 
-    // TODO: Sólo para probar, borrar después !!
-    var fakeList2: List<Feature> = listOf(
-        Feature(Property(2.3, "Spain", Date(), 0, "Prueba1"), Geometry(listOf(1.5f, 2.5f)) )
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEarthquakesBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.searchView.setOnQueryTextListener(this)
         initRecyclerView()
+        getLastEarthquakes()
     }
 
     private fun initRecyclerView() {
-        earthquakeAdapter = EarthquakeAdapter(fakeList2)
+        earthquakeAdapter = EarthquakeAdapter(featureList)
         binding.rvList.layoutManager = LinearLayoutManager(this)
         binding.rvList.adapter = earthquakeAdapter
     }
@@ -47,6 +49,32 @@ class Earthquakes : AppCompatActivity(), OnQueryTextListener {
             .baseUrl("https://earthquake.usgs.gov/fdsnws/event/1/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    private fun getLastEarthquakes() {
+        println("Dentro de getLastEarthquakes")
+        CoroutineScope(Dispatchers.IO).launch {
+            println("Dentro de la corrutina")
+
+            //val call = getRetrofit().create(APIService::class.java).getEarthquakes("query?format=geojson&starttime=${current}&endtime=${endDate}&limit=${selectedPageSize}&offset=${actualOffset}")
+            val call = getRetrofit().create(APIService::class.java).getEarthquakes("query?format=geojson&starttime=2023-01-01&endtime=2023-01-05")
+            println("call OK")
+
+            val features = call.body()
+            runOnUiThread {
+                println("Dentro del runOnUiThread")
+                if (call.isSuccessful) {
+                    val features: List<Feature> = features?.features ?: emptyList()
+                    featureList.clear()
+                    featureList.addAll(features)
+                    earthquakeAdapter.notifyDataSetChanged()
+                    println("API call successfull")
+                } else {
+                    showError()
+                    println("API error")
+                }
+            }
+        }
     }
 
     private fun searchByDate(startTime: String, endTime: String, selectedPageSize: Int, actualOffset: Int) {
