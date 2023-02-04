6353f53fec4857bdbdab60f8756ddd18cb4eb6f2
laurazp <laurikazp@gmail.com>
2023-02-04 15:14:03 +0000

    Parcelable configured ok (with kotlin extensions plugin)

--- app/src/main/java/com/example/quake/API/Models/Geometry.kt
+++ app/src/main/java/com/example/quake/API/Models/Geometry.kt
@@ -1,71 +1,11 @@
 package com.example.quake.API.Models
 
-import android.os.Parcel
 import android.os.Parcelable
-import androidx.core.os.ParcelCompat.readArrayList
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
-
+@Parcelize
 data class Geometry(
-    @SerializedName("coordinates") var coordinates: FloatArray?
-    ) /*: Parcelable {
-    constructor(parcel: Parcel) : this(
-        coordinates = parcel.createStringArrayList()
-        //parcel.readArrayList(Float::class.java.classLoader, Float::class.java)
-        //parcel.readSerializable(Float::class.java.classLoader, Float::class.java) as ArrayList<Float>
-        //parcel.readValue(ArrayList::class.java.classLoader) as ArrayList<Float>
-        //parcel.readList(ArrayList<Float>(), ArrayList::class.java.classLoader, Float::class.java) as ArrayList<Float>
-        //parcel.readList(List<Float>, Float::class.java.classLoader, Float::class.java) as List<Float>
-        //parcel.readList(coordinates, Geometry.class.getClassLoader()) as List<Float>
-        //parcel.readList(coordinates, Geometry::class.java.getClassLoader())
-    ) {
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest?.let {
-            dest.writeStringList(coordinates)
-            //dest.writeSerializable(coordinates) as? ArrayList<Float>
-            //dest.writeValue(coordinates) as ArrayList<Float>
-            //dest.writeList(coordinates)
-        }
-    }
-
-    companion object CREATOR : Parcelable.Creator<Geometry> {
-        override fun createFromParcel(parcel: Parcel): Geometry {
-            return Geometry(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Geometry?> {
-            return arrayOfNulls(size)
-        }
-    }
-}*/ : Parcelable {
-    constructor(parcel: Parcel) : this(
-        coordinates = parcel.createFloatArray()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        //coordinates = parcel.createFloatArray()
-        parcel.writeFloatArray(coordinates)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Geometry> {
-        override fun createFromParcel(parcel: Parcel): Geometry {
-            return Geometry(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Geometry?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    @SerializedName("coordinates") var coordinates: List<Float>
+    ) : Parcelable
 
--- app/src/main/java/com/example/quake/API/Models/Property.kt
+++ app/src/main/java/com/example/quake/API/Models/Property.kt
@@ -1,46 +1,14 @@
 package com.example.quake.API.Models
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import java.sql.Time
-import java.util.Date
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Property (
     @SerializedName("mag") var mag: Double?,
     @SerializedName("place") var place: String?,
     //@SerializedName("time") var time: Date?, //TODO: Search for date/time in Android
-    @SerializedName("tsunami") var tsunami: Int?,
+    @SerializedName("tsunami") var tsunami: Int?, //TODO: Pasar a byte?
     @SerializedName("title") var title: String?
-    ) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Double::class.java.classLoader) as? Double,
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString()
-    ) {
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest?.let {
-            dest.writeValue(mag) as? Double
-            dest.writeString(place)
-            dest.writeInt(tsunami!!)
-            dest.writeString(title)
-        }
-    }
-
-    companion object CREATOR : Parcelable.Creator<Property> {
-        override fun createFromParcel(parcel: Parcel): Property {
-            return Property(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Property?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    ) : Parcelable
